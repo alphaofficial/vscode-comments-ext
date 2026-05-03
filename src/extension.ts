35758c5419299938175030b3b66c35f2f473dfa6
Albert Akrong <alphaxsalt@gmail.com>
2026-05-03 07:23:07 +0100

    scaffold vscode extension project

---
+++ src/extension.ts
@@ -0,0 +1,8 @@
+import * as vscode from "vscode";
+
+export function activate(context: vscode.ExtensionContext): void {
+  // Commands and providers are registered in later tasks.
+  void context;
+}
+
+export function deactivate(): void {}
